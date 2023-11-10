efa9fe31dfb6a4bb2f3ff905c5223d2acf82b4df
alsafrandi <al.safrandi@gmail.com>
2023-11-10 12:42:45 +0700

    Revert "fixing folder"
    This reverts commit d71a7c4ceb957f04eb2793ad43cd1c7f7423884a.

---
+++ Randi/Try.py
@@ -0,0 +1,20 @@
+#for try only
+
+import pandas as pd
+from sklearn import datasets
+
+wine=datasets.load_wine()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.DataFrame(
+    wine.data,
+    columns=wine.feature_names    
+)
+
+sns.countplot(
+    data=df
+)
+plt.title('Wine Data')
+plt.show()
